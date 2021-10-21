23dae4791c2ba3475f72036bf1689d4a3a287723
YmaniN <69920629+YmaniN@users.noreply.github.com>
2021-10-21 09:53:16 -0600

    Add files via upload

---
+++ Definition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CS426.analysis
+{
+	public abstract classs Definition 
+	{
+		public string name;
+
+	public override string ToString()
+		{
+		return name; 
+		}
+	}
+
+public abstract class TypeDefinition : Definition { }
+
+public class NumberDefinitioin : TypeDefinition { }
+
+public class StringDefinition : TypeDefinition { }
+
+public class VariableDefinition : Definition {
+	public TypeDefinition variableType;
+}
+
+public class 
